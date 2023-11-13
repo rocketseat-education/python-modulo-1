2c625cb6cda64ca28aa9067da824e2020782df0f
Tamires Santos <tfs.tamsantos@gmail.com>
2023-11-13 15:51:53 -0300

    Sintaxe

---
+++ Modulo_1/blocos.py
@@ -0,0 +1,6 @@
+idade = 25
+
+if idade < 18:
+    print("menor de idade")
+else:
+    print("maior de idade")
---
+++ Modulo_1/sintaxe.py
@@ -0,0 +1,7 @@
+# Esse é meu primeiro comentário
+""" Esse é meu primeiro comentário
+    Essa é a segunda linha de comentário
+"""
+
+nome = "Gabriel" + \
+" Casemiro"
