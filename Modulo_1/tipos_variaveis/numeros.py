d48b89ed8e5dbd6cfc6d138583986b020ed5474b
Tamires Santos <tfs.tamsantos@gmail.com>
2023-12-22 14:39:05 -0300

    Números

---
+++ Modulo_1/tipos_variaveis/numeros.py
@@ -0,0 +1,43 @@
+# Inteiro
+numero_inteiro = 4
+print("Inteiro = ", numero_inteiro)
+
+# Real com ponto flutuante
+numero_real = 3.14
+print("Real com ponto flutuante = ", numero_real)
+
+# type()
+print("Tipo da variavel inteiro = ", type(numero_inteiro))
+print("Tipo da variavel real = ", type(numero_real))
+
+# soma +
+soma = 1 + 1
+print("1 + 1 = ", soma)
+
+# subtracao -
+subtracao = 1 - 1
+print("1 - 1 = ", subtracao)
+
+# multiplicacao *
+multiplicacao = 2 * 2
+print("2 x 2 = ", multiplicacao)
+
+# divisao /
+divisao = 5 / 2
+print("5 / 2 = ", divisao)
+print("Tipo da variavel do resultado da divisao = ", type(divisao))
+
+# int()
+print("Valor em inteiro = ", int(divisao))
+
+# float()
+print("Valor em float = ", float(divisao))
+
+# Modulo %
+modulo = 5 % 2
+print("Modulo = ", modulo)
+
+# divisao inteiro //
+divisao = 5 // 2
+print("5 // 2 = ", divisao)
+print("Tipo da variavel do resultado da divisao = ", type(divisao))
