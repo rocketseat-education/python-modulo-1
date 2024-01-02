1e4bb7cb30be137d8fff46db210b096ef2d6e3c4
Tamires Santos <tfs.tamsantos@gmail.com>
2024-01-02 15:53:14 -0300

    Principais métodos do dicionário

--- Modulo_1/tipos_variaveis/dicionarios.py
+++ Modulo_1/tipos_variaveis/dicionarios.py
@@ -11,3 +11,25 @@ print("Cidade:", pessoa["cidade"])
 
 pessoa["sobrenome"] = "Silva"
 print("Sobrenome", pessoa["sobrenome"])
+
+pessoa["idade"] = 31
+print("Idade atualizada:", pessoa["idade"])
+
+# Removendo um par chave-valor
+del pessoa["sobrenome"]
+print("Meu dicionário de exemplo:", pessoa)
+
+# Métodos: keys(), values(), items()
+chaves = list(pessoa.keys())
+print("Chaves do dicionário:", chaves)
+print("Primeira chave:", chaves[0])
+
+# Métodos values
+valores = list(pessoa.values())
+print("Valores do dicionário:", valores)
+print("Primeiro valor do dicionário:", valores[0])
+
+# Métodos items
+itens = list(pessoa.items())
+print("Pares chave-valor do dicionário:", itens)
+print("Primeira chave-valor: %s = %s" % (itens[0][0], itens[0][1]))
