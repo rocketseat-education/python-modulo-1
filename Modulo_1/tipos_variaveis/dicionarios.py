014366847de6876b4915f2b356493605f8e9dbf4
Tamires Santos <tfs.tamsantos@gmail.com>
2024-01-02 15:38:16 -0300

    Dicionários

---
+++ Modulo_1/tipos_variaveis/dicionarios.py
@@ -0,0 +1,13 @@
+# Criando um dicionário de exemplo
+pessoa = {"nome": "João", "idade": 30, "cidade": "São Paulo"}
+
+# Exibindo o dicionário
+print("Meu dicionário de exemplo:", pessoa)
+
+# Acessando valores por chave
+print("Nome:", pessoa["nome"])
+print("Idade:", pessoa["idade"])
+print("Cidade:", pessoa["cidade"])
+
+pessoa["sobrenome"] = "Silva"
+print("Sobrenome", pessoa["sobrenome"])
