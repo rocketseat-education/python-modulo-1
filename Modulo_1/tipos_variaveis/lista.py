bee4e2697ae040c677fe67b9badbca11eff3199a
Tamires Santos <tfs.tamsantos@gmail.com>
2024-01-02 14:49:20 -0300

    Listas

---
+++ Modulo_1/tipos_variaveis/lista.py
@@ -0,0 +1,13 @@
+# Declaracao
+
+minha_lista = [1, 2, 3, 4, 5, "rocketseat", True, False]
+
+# Exibindo a lista
+print("Minha lista de exemplo", minha_lista)
+
+# Exibindo a lista
+print("minha_lista[0]:", minha_lista[0])
+print("minha_lista[5]:", minha_lista[5])
+print("minha_lista[1:7]:", minha_lista[1:7]) # minha_lista[1], minha_lista[2], ...minha_lista[6]
+print("minha_lista[:6]:", minha_lista[:6])
+print("minha_lista[2:]:", minha_lista[2:])
