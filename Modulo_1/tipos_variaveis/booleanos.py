0945a7dd013b7f6313891ee1770eef055da4e47b
Tamires Santos <tfs.tamsantos@gmail.com>
2024-01-02 14:38:12 -0300

    Booleanos

---
+++ Modulo_1/tipos_variaveis/booleanos.py
@@ -0,0 +1,34 @@
+# Condição verdadeira
+True
+#Condição Falsa
+False
+
+# Condicionais
+# se condicao
+# else = se não
+if True:
+  print("Bloco IF vai ser executado")
+else: 
+  print("Bloco ELSE não será executado")
+
+# Operadores lógicos: and e or
+
+# AND  
+if True and True:
+  print("Bloco será executado")
+
+if True and False: 
+  print("Bloco não será executado")
+
+if False and False:
+  print("Bloco não será executado")
+
+# OR
+if True or False:
+  print("Bloco OR vai ser executado")
+
+if False or False:
+  print("Bloco OR não vai ser executado")
+
+if True or True:
+  print("Bloco OR vai ser executado")
