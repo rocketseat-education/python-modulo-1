5db5965fd1bb11bdcaadb7bd7786ee90563b2694
Tamires Santos <tfs.tamsantos@gmail.com>
2024-01-03 18:20:45 -0300

    Loop while

---
+++ Modulo_1/projeto/gerenciador.py
@@ -0,0 +1,15 @@
+while True:
+  print("\nMenu do Gerenciador de Lista de tarefas:")
+  print("1. Adicionar tarefa")
+  print("2. Ver tarefas")
+  print("3. Atualizar Tarefas")
+  print("4. Completar Tarefas")
+  print("5. Deletar tarefas completadas")
+  print("6. Sair")
+
+  escolha = input("Digite a sua escolha: ")
+
+  if escolha == "6":
+    break
+
+print("Programa finalizado")
---
+++ Modulo_1/while.py
@@ -0,0 +1,9 @@
+print("Exemplo de while")
+contador = 0
+while True:
+  print("Contagem:", contador)
+  contador += 1
+  if contador == 5:
+    break
+
+print("Programa finalizado")
