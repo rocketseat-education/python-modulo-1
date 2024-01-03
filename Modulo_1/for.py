6f03f9ebd284219750859f823a153b24c3c1aac8
Tamires Santos <tfs.tamsantos@gmail.com>
2024-01-03 17:43:13 -0300

    Continuação de loops for

--- Modulo_1/for.py
+++ Modulo_1/for.py
@@ -21,3 +21,27 @@ print("\nFor utilizando dicionatio - items")
 for chave, valor in pessoa.items():
   print(f"{chave}: {valor}")
 
+# range(): intervalo numérico
+# [0,1,2,3,4]
+print("\n Utilizando a função range()")
+for numero in range(5):
+  print("Numero:", numero)
+
+print("\n Utilizando a função range() com len()")
+lista = [1, 2, 3, 4, 5]
+for indice in range(0, len(lista)):
+  print("Indice:", indice)
+  print("Elemento:", lista[indice])
+  if indice == 3:
+    lista[indice] = 5
+  else:
+    lista[indice] = 0
+print(lista)
+
+# enumerate()
+lista_enumerate = ["a", "b", "c"]
+for indice, valor in enumerate(lista_enumerate):
+  print(f"{indice}: {valor}")
+  if indice == 1:
+    print("Idice 1")
+
