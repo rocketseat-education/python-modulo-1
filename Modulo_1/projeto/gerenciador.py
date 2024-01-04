5c3bf00646ff98057df74aaf7d5525a6a992e665
Tamires Santos <tfs.tamsantos@gmail.com>
2024-01-04 09:49:03 -0300

    Funções do projeto - Atualizar tarefas

--- Modulo_1/projeto/gerenciador.py
+++ Modulo_1/projeto/gerenciador.py
@@ -13,6 +13,16 @@ def ver_tarefas(tarefas):
     status = "✓" if tarefa["completada"] else " "
     nome_tarefa = tarefa["tarefa"]
     print(f"{indice}. [{status}] {nome_tarefa}")
+  return
+
+def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
+  indice_tarefa_ajustado = int(indice_tarefa) - 1
+  if indice_tarefa_ajustado >= 0 and indice_tarefa_ajustado < len(tarefas):
+    tarefas[indice_tarefa_ajustado]["tarefa"] = novo_nome_tarefa
+    print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+  else:
+    print("Índice de tarefas inválido.")
+  return
 
 tarefas = []
 while True:
@@ -31,6 +41,11 @@ while True:
     adicionar_tarefa(tarefas, nome_tarefa)
   elif escolha == "2":
     ver_tarefas(tarefas)
+  elif escolha == "3":
+    ver_tarefas(tarefas)
+    indice_tarefa = input("Digite o número da tarefa que deseja atualizar: ")
+    novo_nome = input("Digite o novo nome da tarefa: ")
+    atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome)
   elif escolha == "6":
     break
 
