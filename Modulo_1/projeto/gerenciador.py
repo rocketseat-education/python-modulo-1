fd8898777d481dc60cfb30730704ce01e531309f
Tamires Santos <tfs.tamsantos@gmail.com>
2024-01-04 09:32:56 -0300

    Funções do projeto - Visualizando tarefas

--- Modulo_1/projeto/gerenciador.py
+++ Modulo_1/projeto/gerenciador.py
@@ -7,6 +7,13 @@ def adicionar_tarefa(tarefas, nome_tarefa):
   print(f"Tarrefa {nome_tarefa} foi adicionada com sucesso!")
   return
 
+def ver_tarefas(tarefas):
+  print("\nLista de tarefas:")
+  for indice, tarefa in enumerate(tarefas, start=1):
+    status = "✓" if tarefa["completada"] else " "
+    nome_tarefa = tarefa["tarefa"]
+    print(f"{indice}. [{status}] {nome_tarefa}")
+
 tarefas = []
 while True:
   print("\nMenu do Gerenciador de Lista de tarefas:")
@@ -22,6 +29,8 @@ while True:
   if escolha == "1":
     nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
     adicionar_tarefa(tarefas, nome_tarefa)
+  elif escolha == "2":
+    ver_tarefas(tarefas)
   elif escolha == "6":
     break
 
